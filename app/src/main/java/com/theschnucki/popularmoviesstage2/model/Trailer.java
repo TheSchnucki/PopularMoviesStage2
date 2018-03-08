40b5d0d7e07d1eecbf19437635291182f67fafe1
theSchnucki <mark.schneider@gmx.net>
2018-03-08 07:50:49 +0100

    Added "Review" to Models Changed the "trailer_item" design because icons had different size made the "Trailer" properties private

--- app/src/main/java/com/theschnucki/popularmoviesstage2/model/Trailer.java
+++ app/src/main/java/com/theschnucki/popularmoviesstage2/model/Trailer.java
@@ -4,10 +4,10 @@ public class Trailer {
 
     public static final String TAG = Trailer.class.getSimpleName();
 
-    public String key;
-    public String name;
-    public String type;
-    public String path;
+    private String key;
+    private String name;
+    private String type;
+    private String path;
 
     public String getKey() {
         return key;
